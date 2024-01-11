d8f36c0d2b9fb0c8bfc3bd03d44c5847d1554a23
eugenelijondo <153195948+eugenelijondo@users.noreply.github.com>
2024-01-11 18:56:12 +0300

    Add files via upload

---
+++ phase-1-sending-data-with-fetch/index.js
@@ -0,0 +1,31 @@
+// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email,
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        document.body.innerHTML = object.id;
+      })
+      .catch(function (error) {
+        document.body.innerHTML = error.message;
+      });
+  }
+  
+  // Test the submitData function
+  submitData("John Doe", "john@example.com");
+  
